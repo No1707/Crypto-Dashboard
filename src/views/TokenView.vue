72dc3cf0067f4423b879fca23566cb223518ef56
No1707 <nolanwebpro@gmail.com>
2022-05-12 21:10:17 +0200

    .

--- src/views/TokenView.vue
+++ src/views/TokenView.vue
@@ -1,50 +1,58 @@
 <template>
-  <div class="w-full pb-5 px-5 flex flex-col justify-between bg-slate-200 text-slate-700 dark:bg-slate-700 dark:text-white" v-if="data">
-    <div class="mx-4  my-8 flex">
-      <!-- Go back button -->
-      <button @click="$router.go(-1)" class="self-start text-xl">&lt; Back</button>
+  <div v-if="data">
 
-      <!-- Logo -->
-      <img class="ml-12 mr-6" :src="data.image.small" alt="Token logo">
+    <div class="w-full pb-5 px-5 flex flex-col justify-between bg-slate-200 text-slate-700 dark:bg-slate-700 dark:text-white">
+
+      <div class="mx-4  my-8 flex">
+        <!-- Go back button -->
+        <button @click="$router.go(-1)" class="self-start text-xl">&lt; Back</button>
+
+        <!-- Logo -->
+        <img class="ml-12 mr-6" :src="data.image.small" alt="Token logo">
+        
+        <!-- Name -->
+        <h1 class="font-bold text-5xl uppercase"> {{ token }} </h1>
+      </div>
+
+      <div class="flex justify-between px-14">
+        <div class="bg-white dark:bg-slate-800 p-4 rounded shadow-lg w-full m-6">
+          <div class="flex flex-col min-w-full">
+            <h2 class="text-slate-400 uppercase font-bold text-sm ">price</h2>
+            <p class="font-bold text-xl">${{ data.market_data.current_price.usd }}</p>
+          </div>
+        </div>
+        <div class="bg-white dark:bg-slate-800 p-4 rounded shadow-lg w-full m-6">
+          <div class="flex flex-col">
+            <h2 class="text-slate-400 uppercase font-bold text-sm ">market cap.</h2>
+            <p class="font-bold text-xl">${{ data.market_data.market_cap.usd.toLocaleString() }}</p>
+          </div>
+        </div>
+        <div class="bg-white dark:bg-slate-800 p-4 rounded shadow-lg w-full m-6">
+          <div class="flex flex-col">
+            <h2 class="text-slate-400 uppercase font-bold text-sm ">circulating supply</h2>
+            <p class="font-bold text-xl">{{ data.market_data.circulating_supply.toLocaleString() }} tokens</p>
+          </div>
+        </div>
+        <div class="bg-white dark:bg-slate-800 p-4 rounded shadow-lg w-full m-6">
+          <div class="flex flex-col" v-if="data.market_data.fully_diluted_valuation.usd !== undefined">
+            <h2 class="text-slate-400 uppercase font-bold text-sm ">fully diluted valuation</h2>
+            <p class="font-bold text-xl">${{ data.market_data.fully_diluted_valuation.usd.toLocaleString() }}</p>
+          </div>
+          <div class="flex flex-col" v-else>
+            <h2 class="text-slate-400 uppercase font-bold text-sm ">total volume</h2>
+            <p class="font-bold text-xl">${{ data.market_data.total_volume.usd.toLocaleString() }}</p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="px-8 pt-4 flex flex-col justify-center items-center">
+      <h3 class="font-bold text-2xl underlined"><span class="capitalize">{{token}}</span> description :</h3>
+      <p class="description">Desc ?</p>
+    </div>
       
-      <!-- Name -->
-      <h1 class="font-bold text-5xl uppercase"> {{ token }} </h1>
-    </div>
 
-    <div class="flex justify-between px-14">
-      <div class="bg-white dark:bg-slate-800 p-4 rounded shadow-lg w-full m-6">
-        <div class="flex flex-col min-w-full">
-          <h2 class="text-slate-400 uppercase font-bold text-sm ">price</h2>
-          <p class="font-bold text-xl">${{ data.market_data.current_price.usd }}</p>
-        </div>
-      </div>
-      <div class="bg-white dark:bg-slate-800 p-4 rounded shadow-lg w-full m-6">
-        <div class="flex flex-col">
-          <h2 class="text-slate-400 uppercase font-bold text-sm ">market cap.</h2>
-          <p class="font-bold text-xl">${{ data.market_data.market_cap.usd.toLocaleString() }}</p>
-        </div>
-      </div>
-      <div class="bg-white dark:bg-slate-800 p-4 rounded shadow-lg w-full m-6">
-        <div class="flex flex-col">
-          <h2 class="text-slate-400 uppercase font-bold text-sm ">circulating supply</h2>
-          <p class="font-bold text-xl">{{ data.market_data.circulating_supply.toLocaleString() }} tokens</p>
-        </div>
-      </div>
-      <div class="bg-white dark:bg-slate-800 p-4 rounded shadow-lg w-full m-6">
-        <div class="flex flex-col" v-if="data.market_data.fully_diluted_valuation.usd !== undefined">
-          <h2 class="text-slate-400 uppercase font-bold text-sm ">fully diluted valuation</h2>
-          <p class="font-bold text-xl">${{ data.market_data.fully_diluted_valuation.usd.toLocaleString() }}</p>
-        </div>
-        <div class="flex flex-col" v-else>
-          <h2 class="text-slate-400 uppercase font-bold text-sm ">total volume</h2>
-          <p class="font-bold text-xl">${{ data.market_data.total_volume.usd.toLocaleString() }}</p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="px-8 pt-4 flex flex-col justify-center items-center">
-    <h3 class="font-bold text-2xl underlined"><span class="capitalize">{{token}}</span> description :</h3>
-    <p class="description">Desc ?</p>
   </div>
 </template>
 
@@ -75,3 +83,15 @@ import { useRoute } from 'vue-router'
     }
   }
 </script>
+
+<style scoped>
+  .tokenBg::before{
+    /* content: '';
+    background-image: v-bind('url('data.image.large')');
+    background-size: cover;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    z-index: -1; */
+  }
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   content: ['./public/index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      backgroundSize: {
+        '100%': '100%',
+        '250%': '250%',
+      },
+      zIndex: {
+        '-1': '-1'
+      }
+    },
   },
   plugins: [],
 }
